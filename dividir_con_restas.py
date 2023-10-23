fc78e5e2bca11d998ddaadc1d7d69d16ebf3a6fd
Moni-ib <monica.ibarra@iteso.mx>
2023-10-22 18:55:34 -0600

    while

--- dividir_con_restas.py
+++ dividir_con_restas.py
@@ -4,14 +4,15 @@ Inserta el encabezado aquí y escribe tu código abajo
 
 # Declaraciones
 cociente = 0
+residuo = 0
 
 # Entradas
 dividendo = int(input("Introduzca el dividendo: "))
 divisor = int(input("Introduzca el divisor: "))
 
 # Proceso
-residuo = dividendo
-while residuo >= divisor:
+
+while residuo + divisor <= dividendo:
     residuo -= divisor
     cociente += 1
 
